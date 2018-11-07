34da2054de4e37d59992cdf4066577a4ae2625fa
Nelson Portilla <nelson.herrera@correounivalle.edu.co>
2018-11-07 14:40:49 -0500

    dim_perfil_incidencia
    Creando Perfil de Incidencia

--- sql/query_dim_localizaciones_cie10_who.sql
+++ sql/query_dim_localizaciones_cie10_who.sql
@@ -12,5 +12,5 @@ cie.loc,
 substring(cie.cie_10 from 1 for 3) as subcie
 from "Loc" as loca left join dict_cie10cancer as cie on loca."Localizacion"=cie.loc 
 ) as subcie10 on who.icd10=subcie
-n=482
+--n=482
 --select * from "Loc" as loc left join dict_cie10cancer as dic on loc."Localizacion"=dic.loc 
